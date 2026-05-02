76a12a51ccff28fc875745ddcd2fce37d5bf7f10
Richard-ZJC <u3648101@connect.hku.hk>
2026-05-02 12:23:07 +0800

    Your commit message describing the changes

--- src/shop.cpp
+++ src/shop.cpp
@@ -145,8 +145,8 @@ void Shop::showShopUI() {
     while (isShopOpen) {
         merchant->showGoodsList();
         std::cout << "\n[Shop] Choose an option:\n";
-        std::cout << "1. Buy Sword (+10 ATK)\n";
-        std::cout << "2. Upgrade Potion (+10~23 HP)\n";
+        std::cout << "1. Upgrade Sword (+10 ATK)\n";
+        std::cout << "2. Buy Potion (+10~23 HP)\n";
         std::cout << "3. Upgrade Armor (+10 DEF)\n";
         std::cout << "0. Leave Shop\n";
         std::cout << "Choice: ";
