8954f67c4fe6670cad636d9513658c68a6a28cc9
Jin Haoyue <271463130+hyjin215@users.noreply.github.com>
2026-05-02 18:26:01 +0800

    add comments on item.h

--- include/item.h
+++ include/item.h
@@ -9,6 +9,10 @@
 // Forward declaration
 class Player;
 
+/**
+ * @struct ItemData
+ * @brief Data structure to store all core attributes of an item
+ */
 struct ItemData {
     std::string name;
     ItemType type;
@@ -18,31 +22,104 @@ struct ItemData {
     bool isConsumed;
 };
 
+/**
+ * @brief Global item database that maps item ID to ItemData
+ */
 extern std::unordered_map<int, ItemData> itemDatabase;
 
+/**
+ * @brief Initializes the global item database with default items
+ * @return None
+ */
 void initItemDatabase();
 
-// Item :only id
+/**
+ * @class Item
+ * @brief Item class that uses ID to reference item data from the database
+ */
 class Item {
 private:
     int id;
 
 public:
+    /**
+     * @brief Default constructor
+     */
     Item();
+
+    /**
+     * @brief Constructor with item ID
+     * @param id - Unique integer ID of the item
+     */
     explicit Item(int id);           
-    
+
+    /**
+     * @brief Gets the item ID
+     * @return int - Item ID
+     */
     int getId() const;
+
+    /**
+     * @brief Gets the item name from the database
+     * @return std::string - Item name
+     */
     std::string getName() const;
+
+    /**
+     * @brief Gets the item type (WEAPON, ARMOR, POTION, etc.)
+     * @return ItemType - Type of the item
+     */
     ItemType getType() const;
+
+    /**
+     * @brief Gets the item rarity (LOW, MEDIUM, HIGH)
+     * @return ItemRarity - Rarity level
+     */
     ItemRarity getRarity() const;
+
+    /**
+     * @brief Gets the effect value of the item
+     * @return float - Numeric effect value
+     */
     float getEffectValue() const;
+
+    /**
+     * @brief Gets the item price
+     * @return int - Item price in gold
+     */
     int getPrice() const;
+
+    /**
+     * @brief Checks if the item is consumed on use
+     * @return bool - True if consumed, false otherwise
+     */
     bool getIsConsumed() const;
+
+    /**
+     * @brief Sets whether the item is consumed when used
+     * @param state - Boolean consumption state
+     * @return None
+     */
     void setIsConsumed(bool state);
-    
+
+    /**
+     * @brief Applies the item's effect to the player
+     * @param player - Reference to the player object
+     * @return None
+     */
     void applyEffect(Player& player);
+
+    /**
+     * @brief Displays complete item information to the console
+     * @return None
+     */
     void displayItemInfo() const;
-    
+
+    /**
+     * @brief Equality operator to compare two items by ID
+     * @param other - Another Item object to compare
+     * @return bool - True if IDs are equal
+     */
     bool operator==(const Item& other) const;
 };
 
