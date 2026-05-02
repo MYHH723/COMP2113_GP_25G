8869385dc4245785a9006018998e5b8b03ede02e
Jin Haoyue <271463130+hyjin215@users.noreply.github.com>
2026-05-02 17:44:14 +0800

    add comments on savegame.h

--- include/savegame.h
+++ include/savegame.h
@@ -2,9 +2,12 @@
 #ifndef SAVEGAME_H
 #define SAVEGAME_H
 
+// Include JSON library for game data serialization and deserialization
 #include "third_party/json/single_include/nlohmann/json.hpp"
+
+// Alias for JSON library to simplify code usage
 using json = nlohmann::json;
 
-// 全局函数声明已删除，因为实现是 Game 的成员函数
+// Global functions removed - save/load logic is now implemented as member functions of Game class
 
 #endif
